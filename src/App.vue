c63fae61f2af6d74f80524548b836960e9d58831
Ricardo Sacramento <ricardo.sacramento18@gmail.com>
2022-03-31 23:34:54 +0100

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Dashboard</RouterLink>
+        <RouterLink to="/budget">Budget</RouterLink>
+        <RouterLink to="/categories">Categories</RouterLink>
+        <RouterLink to="/import">import</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+@import 'assets/base.css';
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+    </template>
+    <template #heading>Support Vue</template>
+
+    As an independent project, Vue relies on community backing for its sustainability. You can help
+    us by
+    <a target="_blank" href="https://vuejs.org/sponsor/">becoming a sponsor</a>.
+  </WelcomeItem>
+</template>
